29cef8806afffc774e04228cec85239dd98d682a
HarminderK <harminderk@hotmail.ca>
2017-05-15 14:37:55 -0700

    Add files via upload

---
+++ RGB_Analog_Output.ino
@@ -0,0 +1,71 @@
+/*
+ * Analog output of RGB Led using Potentiometer
+ * 
+ *  Notes: 
+ *  Each analog output needs an output value greater 
+ *  than ~135 for the individual colour to turn on.
+ *  
+ *  There doesn't seem to be a "max" value for the analog output,
+ *  using 255 because of the colour graph.
+ */
+
+
+#define POT A0
+#define BLUE A1
+#define GREEN A2
+#define RED A3
+#define MAX 255
+#define MIN 0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(POT, INPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  
+  analogWrite(BLUE, MIN);
+  analogWrite(GREEN, MIN);
+  analogWrite(RED, MIN);
+
+  Serial.begin(9600); //testing purposes
+  
+}
+
+void loop() {
+int value = analogRead(POT);
+Serial.println(analogRead(POT));
+
+if(value <= MAX && value >= MIN) 
+{
+  analogWrite(BLUE, value);
+}
+
+if(value <= MAX*2 && value > MAX) 
+{
+  analogWrite(BLUE, 2*MAX - value);
+  analogWrite(GREEN, value - MAX);
+}
+
+if(value <= MAX*3 && value > MAX*2) 
+{
+  analogWrite(GREEN, 3*MAX - value);
+  analogWrite(RED, value - 2*MAX);
+}
+
+if(value <= 1020 && value > MAX*3) 
+{
+  analogWrite(BLUE, value - 3*MAX);
+  analogWrite(GREEN, value - 3*MAX);
+  analogWrite(RED, value - 3*MAX);
+}
+
+if(value <= 1023 && value > 1020) 
+{
+  analogWrite(BLUE, MAX);
+  analogWrite(GREEN, MAX);
+  analogWrite(RED, MAX);
+}
+
+
+}
---
+++ RGB_Digital_Output.ino
@@ -0,0 +1,66 @@
+
+/*
+ * Controlling a RGB Led with a Potentiometer (Digital)
+ * 
+ * 
+ * 
+ * 
+ */
+  
+
+
+#define B 13 // Digital Blue Output
+#define G 12 // Digital Green Output
+#define R 11 // Digital Red Output
+#define RGB A0 // Potentiometer
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(B, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(R, OUTPUT);
+  pinMode(RGB, INPUT);
+
+  Serial.begin(9600); //testing purposes
+  
+}
+
+// Changing the colour of the RGB with the potentiometer
+void loop() {
+  int value = analogRead(RGB);
+  if ( value <= 205) {
+  digitalWrite(B, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(R, LOW);
+  }
+
+  if ( value >= 360 && value <= 460) {
+  digitalWrite(B, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(R, LOW);
+  }
+  
+
+  if ( value >= 460 && value <= 770) {
+  digitalWrite(B, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(R, LOW);
+  }
+
+   if ( value >= 770 && value <= 870) {
+  digitalWrite(B, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(R, HIGH);
+  }
+
+   if ( value >= 870 && value <= 1023) {
+  digitalWrite(B, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(R, HIGH);
+  }
+
+ 
+  Serial.println(analogRead(RGB)); // testing purposes
+
+}
